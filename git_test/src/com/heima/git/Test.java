97a565a53aff8b4f97a735c1b474e172fa59294c
lisi <lisi@itcast.cn>
2019-06-30 19:33:24 +0800

    修改本地工程添加到git

--- git_test/src/com/heima/git/Test.java
+++ git_test/src/com/heima/git/Test.java
@@ -10,4 +10,9 @@ public class Test {
         System.out.println("hello git");
         System.out.println("hello git");
     }
+    public void test(){
+        int i=1;
+        i+=10;
+        System.out.println(i);
+    }
 }
