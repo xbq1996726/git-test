12d5fd0c32cdc63c749aab173d3546912978b5db
lisi <lisi@itcast.cn>
2019-06-30 19:24:41 +0800

    本地工程添加到git

---
+++ git_test/src/com/heima/git/Test.java
@@ -0,0 +1,13 @@
+package com.heima.git;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
