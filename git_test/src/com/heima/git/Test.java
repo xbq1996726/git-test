31bc3a598dc26d5dbdd6e513b6a2d05f957fcf6d
lisi <lisi@itcast.cn>
2019-06-30 19:52:29 +0800

    再次修改

--- git_test/src/com/heima/git/Test.java
+++ git_test/src/com/heima/git/Test.java
@@ -9,6 +9,7 @@ public class Test {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //hello
     }
     public void test(){
         int i=1;
